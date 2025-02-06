7f1cc41fff90da0c5420970a57d3b707086cead5
BreinerSan <breiner.sv@gmail.com>
2025-02-05 22:45:00 -0500

    Usando archivos esraricos

--- src/components/home/Description/Desciption.tsx
+++ src/components/home/Description/Desciption.tsx
@@ -1,7 +1,17 @@
 export const Description = () => {
 	return (
 		<section>
-			<h1>Description</h1>
+			<img src="/images/description.jpeg" alt="products marquetplace" />
+			<h2>Description</h2>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus! Doloremque, doloremque
+				doloremque, voluptatibus, quisquam, doloremque dolores, voluptas, quos, quis, doloribus, consequatur
+				voluptatem, quisquam, voluptatibus, doloremque, voluptas, quos, quis, doloribus, consequatur voluptatem,
+				quisquam, voluptatibus, doloremque, voluptas, quos, quis, doloribus, consequatur voluptatem, quisquam,
+				voluptatibus, doloremque, voluptas, quos, quis, doloribus, consequatur voluptatem, quisquam, voluptatibus,
+				doloremque, voluptas, quos, quis, doloribus, consequatur voluptatem, quisquam, voluptatibus, doloremque,
+				voluptas, quos, quis, doloribus, consequatur voluptatem, quisquam, voluptatibus, dolor
+				</p>
 		</section>
 	);
 };

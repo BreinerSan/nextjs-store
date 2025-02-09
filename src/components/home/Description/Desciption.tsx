8c6ceb16cbffc7b5e06b937e6598f6ece70d4346
BreinerSan <breiner.sv@gmail.com>
2025-02-08 23:06:42 -0500

    Manejo y optimizando imagenes con next Image

--- src/components/home/Description/Desciption.tsx
+++ src/components/home/Description/Desciption.tsx
@@ -1,17 +1,28 @@
+import styles from './Description.module.sass';
+import Image from 'next/image';
+
 export const Description = () => {
 	return (
-		<section>
-			<img src="/images/description.jpeg" alt="products marquetplace" />
-			<h2>Description</h2>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus! Doloremque, doloremque
-				doloremque, voluptatibus, quisquam, doloremque dolores, voluptas, quos, quis, doloribus, consequatur
-				voluptatem, quisquam, voluptatibus, doloremque, voluptas, quos, quis, doloribus, consequatur voluptatem,
-				quisquam, voluptatibus, doloremque, voluptas, quos, quis, doloribus, consequatur voluptatem, quisquam,
-				voluptatibus, doloremque, voluptas, quos, quis, doloribus, consequatur voluptatem, quisquam, voluptatibus,
-				doloremque, voluptas, quos, quis, doloribus, consequatur voluptatem, quisquam, voluptatibus, doloremque,
-				voluptas, quos, quis, doloribus, consequatur voluptatem, quisquam, voluptatibus, dolor
-				</p>
+		<section className={styles.Description}>
+			<Image 
+				src="/images/description.jpeg" 
+				alt="products marquetplace" 
+				width={500} 
+				height={300}
+				priority={false}
+				/>
+			<div className={styles.Description__text}>
+				<h2>Description</h2>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus! Doloremque, doloremque
+					doloremque, voluptatibus, quisquam, doloremque dolores, voluptas, quos, quis, doloribus, consequatur
+					voluptatem, quisquam, voluptatibus, doloremque, voluptas, quos, quis, doloribus, consequatur voluptatem,
+					quisquam, voluptatibus, doloremque, voluptas, quos, quis, doloribus, consequatur voluptatem, quisquam,
+					voluptatibus, doloremque, voluptas, quos, quis, doloribus, consequatur voluptatem, quisquam, voluptatibus,
+					doloremque, voluptas, quos, quis, doloribus, consequatur voluptatem, quisquam, voluptatibus, doloremque,
+					voluptas, quos, quis, doloribus, consequatur voluptatem, quisquam, voluptatibus, dolor
+					</p>
+			</div>
 		</section>
 	);
 };
